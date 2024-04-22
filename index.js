1d1bc20137235b6e5974efbe3a42f8347ee92ccf
killerzero337 <metalmaster37yt@gmail.com>
2024-04-22 21:19:38 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import fs from "fs";
+
+const app = express();
+const readData = (nombreFichero) => {
+    const data = fs.readFileSync(`./${nombreFichero}.json`);
+    return JSON.parse(data);
+}
+
+
+app.get("/", (req, res) => {
+    res.send("Hola mundo");
+})
+
+app.get("/radiacion", (req, res) => {
+    const radiacion = readData("radiacionSolar");
+    res.json(radiacion);
+})
+
+
+app.listen(3000, () => console.log("Server running on port 3000"));
